955fde6f52fe9bbf92259d7ef6165fb5cdfc5e32
Edson Tirelli <ed.tirelli@gmail.com>
2014-05-17 21:46:50 -0400

    Simplifying example even more

--- kie-server/src/main/java/org/kie/server/api/KieServer.java
+++ kie-server/src/main/java/org/kie/server/api/KieServer.java
@@ -5,19 +5,14 @@ import java.util.List;
 import javax.jws.WebParam;
 import javax.jws.WebParam.Mode;
 import javax.jws.WebService;
-import javax.xml.ws.RequestWrapper;
-
-import org.kie.api.builder.ReleaseId;
 
 @WebService
 public interface KieServer {
     
-    @RequestWrapper(className="org.drools.compiler.kproject.ReleaseIdImpl")
-    public ServiceResponse deployModule( @WebParam(name="releaseId", mode=Mode.IN) ReleaseId releasedId );
+    public String deployModule( @WebParam(name="releaseId", mode=Mode.IN) String releasedId );
     
-    public List<ReleaseId> getModules();
+    public List<String> getModules();
     
-    @RequestWrapper(className="org.drools.compiler.kproject.ReleaseIdImpl")
-    public ServiceResponse undeployModule( @WebParam(name="deploymentId", mode=Mode.IN) ReleaseId deploymentId );
+    public String undeployModule( @WebParam(name="deploymentId", mode=Mode.IN) String deploymentId );
 
 }
--- kie-server/src/main/java/org/kie/server/impl/KieServerImpl.java
+++ kie-server/src/main/java/org/kie/server/impl/KieServerImpl.java
@@ -5,30 +5,27 @@ import java.util.List;
 
 import javax.jws.WebService;
 
-import org.drools.compiler.kproject.ReleaseIdImpl;
-import org.kie.api.builder.ReleaseId;
 import org.kie.server.api.KieServer;
-import org.kie.server.api.ServiceResponse;
 
 @WebService(endpointInterface="org.kie.server.api.KieServer", serviceName="KieServer")
 public class KieServerImpl implements KieServer {
 
     @Override
-    public ServiceResponse deployModule(ReleaseId releaseId) {
+    public String deployModule(String releaseId) {
         System.out.println(">>> deploy "+releaseId);
-        return new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "KJar "+releaseId+" successfully deployed.");
+        return "SUCCESS"; //new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "KJar "+releaseId+" successfully deployed.");
     }
 
     @Override
-    public List<ReleaseId> getModules() {
+    public List<String> getModules() {
         System.out.println(">>> list modules");
-        return Arrays.asList((ReleaseId)new ReleaseIdImpl("org.foo","bar","1.0.0"), new ReleaseIdImpl("bax.baz","foo", "5.4.3-SNAPSHOT"));
+        return Arrays.asList("org.foo:bar:1.0.0", "bax.baz:foo:5.4.3-SNAPSHOT");
     }
 
     @Override
-    public ServiceResponse undeployModule(ReleaseId releaseId) {
+    public String undeployModule(String releaseId) {
         System.out.println("--- undeploy "+releaseId);
-        return new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "KJar "+releaseId+" successfully undeployed.");
+        return "SUCCESS"; //new ServiceResponse(ServiceResponse.ResponseType.SUCCESS, "KJar "+releaseId+" successfully undeployed.");
     }
 
 }
